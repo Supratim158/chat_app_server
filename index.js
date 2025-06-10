2a92095eca95505a248fbc19a4ddce2b7e611e28
Supratim158 <supratim2005k@gmail>
2025-06-10 14:31:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+var http = require("http");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server, {
+    cors: {
+    origin: "*",
+    },
+});
+
+//middlewre
+
+app.use(express.json());
+app.use(cors());
+
+io.on("Connection", (socket) => {
+    console.log("connection");
+});
+
+server.listen(port, () => {
+    console.log("Server Started");
+});
