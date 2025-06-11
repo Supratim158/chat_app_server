81e0a817b3457bb6a38ba4023b866e40474c4e33
Supratim158 <supratim2005k@gmail>
2025-06-11 14:29:04 +0530

    deployment changes

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const express = require("express");
 var http = require("http");
 const cors = require("cors");
+const { log } = require("console");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require("socket.io")(server, {
-    cors: {
-    origin: "*",
-    },
-});
+var io = require("socket.io")(server);
 
 //middlewre
 
 app.use(express.json());
+var clients = {};
+
 app.use(cors());
 
-io.on("Connection", (socket) => {
+
+io.on("connection", (socket) => {
     console.log("connection");
+    console.log(socket.id, "has joined");
+    socket.on("signin", (id)=>{
+        console.log(id);
+        clients[id]=socket;
+    });
+    socket.on("message",(msg)=>{
+        console.log(msg);
+        let targetId = msg.targetId;
+        if (clients[targetId]) clients[targetId].emit("message",msg);
+    })
+});
+app.route("/check").get((req,res)=>{
+    return res.json("Your aoo is working fine");
 });
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
     console.log("Server Started");
 });
